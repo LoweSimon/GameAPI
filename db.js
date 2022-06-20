7e425e4bcdb266882b5d55849e5df0b07d6598ba
Simon <40033923+LoweSimon@users.noreply.github.com>
2022-06-20 13:56:01 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://simon:Natalie.1990@gamecluster.dv05h.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb://simon:<DB Password to be entered>@gamecluster.dv05h.mongodb.net/?retryWrites=true&w=majority')
